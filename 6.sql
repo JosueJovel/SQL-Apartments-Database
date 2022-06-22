ba593394d9973edec61fa3b5a7a918c682b0519a
Josue Jovel <105612429+ClutchyMilk@users.noreply.github.com>
2022-06-22 04:24:46 -0500

    Delete 6.sql

--- 6.sql
+++
@@ -1,3 +0,0 @@
-SELECT Apartment_Number, Direction, Nickname, SUBSTR(TO_CHAR(Apartment_Number), 1, 1) AS Floor, Base_Price + (100 * COUNT(FEATURE)) AS Rental_Price, Bathroom_Color, Carpet_Color, Kitchen_Color
-FROM Empty_Apartments NATURAL JOIN Apartments NATURAL JOIN Features NATURAL JOIN Buildings NATURAL JOIN Floor_Plan
-GROUP BY Apartment_Number, Direction, Nickname, Base_Price, Bathroom_Color, Carpet_Color, Kitchen_Color;
