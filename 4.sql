7d2a9aa4f45a1c6258e0fffe02ae888ec89cf19a
Josue Jovel <105612429+ClutchyMilk@users.noreply.github.com>
2022-06-22 04:24:14 -0500

    Delete 4.sql

--- 4.sql
+++
@@ -1,3 +0,0 @@
-SELECT Construction_Permit, Floors, Construction_Year, Inspector_First_Name, Inspector_Last_Name, Date_Inspected FROM Buildings NATURAL JOIN New_Buildings NATURAL JOIN Inspections
-WHERE Result LIKE 'F'
-ORDER BY Date_Inspected ASC;
