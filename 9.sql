ca96c41c7935026432f60b9a3572af6bb8aaaa5f
Josue Jovel <105612429+ClutchyMilk@users.noreply.github.com>
2022-06-22 04:25:24 -0500

    Delete 9.sql

--- 9.sql
+++
@@ -1,5 +0,0 @@
-SELECT CONCAT(Bedrooms, Plan_Letter) AS Floorplan, Bedrooms, Bathrooms, Base_Price + (100 * COUNT(Feature)) AS Rental_Price, Area 
-FROM Apartments NATURAL JOIN Features NATURAL JOIN Floor_Plan NATURAL JOIN Tenants LEFT JOIN Tenant_References ON Tenants.Tenant_ID = Tenant_References.Tenant_ID
-WHERE Reference_Phone IS NULL
-GROUP BY Plan_Letter, Bedrooms, Bathrooms, Base_Price, Area
-ORDER BY Area DESC;
